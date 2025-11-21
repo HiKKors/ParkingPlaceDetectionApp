59386716bf2ef1737a5136237c09145bc15bd36e
HiKKors <nk041496@gmail.com>
2025-11-21 23:08:03 +0400

    added blocklist for logout

--- AuthService/app/extensions.py
+++ AuthService/app/extensions.py
@@ -1,5 +1,12 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
+# from .models import User
+from .blocklist import BLOCKLIST
 
 db = SQLAlchemy()
 jwt = JWTManager()
+
+@jwt.token_in_blocklist_loader
+def check_if_token_revoked(jwt_header, jwt_payload):
+    jti = jwt_payload["jti"]
+    return jti in BLOCKLIST
